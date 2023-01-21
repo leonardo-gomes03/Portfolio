e4cff65d37c6b8fca1a506682148b46598c52c38
leonardo-gomes03 <leogomes.pereira03@gmail.com>
2023-01-21 15:46:14 -0300

    feat: colocando funcionalidade nos botões

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
+//Declarando const's
+
 const openModalButton = document.querySelector("#open-modal");
 const closeModalButton = document.querySelector("#close-modal");
 const modal = document.querySelector("#modal");
 const fade = document.querySelector("#fade");
+const btnHabilidades = document.getElementById("btn-habilidades");
+const contentHabilidades = document.getElementById("content-habilidades")
+const btnProjetos = document.getElementById("btn-projetos");
+const contentProjetos = document.getElementById("content-projetos")
+const btnSobremim = document.getElementById("btn-sobremim");
+const contentSobremim = document.getElementById("content-sobremim")
+
+//Declarando arrays
+
+const inputBtn = [
+  btnHabilidades,
+  btnProjetos,
+  btnSobremim
+];
+
+const outputBtn = [
+  contentHabilidades,
+  contentProjetos,
+  contentSobremim
+];
+
+//Função do modal
 
 const toggleModal = () => {
   modal.classList.toggle("hide");
@@ -9,6 +33,17 @@ const toggleModal = () => {
   console.log("click");
 };
 
+//Função de Ação do modal
+
 [openModalButton, closeModalButton, fade].forEach((el) => {
   el.addEventListener("click", () => toggleModal());
 });
+
+//Função ir para elemento desejado
+
+inputBtn.forEach((el) => {
+  el.addEventListener("click", () => {
+    const catchIndex = inputBtn.indexOf(el);
+    outputBtn.at(catchIndex).scrollIntoView();
+  })
+})
